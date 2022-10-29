4e68506665cfa42e2fb551443d6068e8d519572c
Nehalokesh <nehal19697@gmail.com>
2022-10-29 19:02:48 +0530

    task1 commit

---
+++ sign.js
@@ -0,0 +1,8 @@
+function signup(event){
+    event.preventDefault()
+    const signupDetails = {
+        email:event.target.email.value,
+        password:event.target.password.value
+    }
+    console.log(signupDetails)
+}

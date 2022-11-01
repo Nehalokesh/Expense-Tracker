cde887ebac27dc156441a4f35db61568325b0351
Nehalokesh <nehal19697@gmail.com>
2022-11-01 12:30:37 +0530

    task8 commit

--- routes/expense.js
+++ routes/expense.js
@@ -8,6 +8,8 @@ const userauthenticate = require('../middleware/auth')
 
 const controller = require('../controllers/expense')
 
+const purchasecontroller = require('../controllers/purchase')
+
 router.post('/signup',usercontroller.signup)
 
 router.post('/login',usercontroller.login)
@@ -17,6 +19,10 @@ router.post('/details',userauthenticate.authenticate,controller.postdetails)
 router.get('/userinfo',userauthenticate.authenticate,controller.getdetails)
 
 router.delete('/delete/:id',userauthenticate.authenticate,controller.delete);
+
+router.get('/purchase',userauthenticate.authenticate,purchasecontroller.purchasepremium)
+
+router.post('/updatepurchase',userauthenticate.authenticate, purchasecontroller.updateTransactionStatus)
 
 module.exports = router
 
